d5868390857ad834208ced19e9b46396b51f7ba4
Ilan <ilan.servais@laplateforme.io>
2025-06-09 00:51:14 +0200

    fix: add NEXT_PUBLIC_TMDB_API_KEY to environment configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   // expose your API URL to the client
   env: {
+    NEXT_PUBLIC_TMDB_API_KEY: process.env.NEXT_PUBLIC_TMDB_API_KEY,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   
